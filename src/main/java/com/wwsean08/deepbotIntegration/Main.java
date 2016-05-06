d4edf93bf969aff19e398a692489db263763a28b
Sean Smith <sean@wwsean08.com>
2016-05-05 23:50:54 -0700

    Updated to POST data to endpoint for consumption

--- src/main/java/com/wwsean08/deepbotIntegration/Main.java
+++ src/main/java/com/wwsean08/deepbotIntegration/Main.java
@@ -1,9 +1,18 @@
 package com.wwsean08.deepbotIntegration;
 
+import com.google.gson.Gson;
 import com.wwsean08.deepbotIntegration.pojo.User;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 
+import java.io.StringBufferInputStream;
 import java.net.URI;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -15,6 +24,7 @@ public class Main
 {
     public static void main(String[] args) throws Exception
     {
+        long startTime = System.currentTimeMillis();
         String destUri = "ws://10.0.0.10:3337";
         WebSocketClient client = new WebSocketClient();
         GetUsersSocket socket = new GetUsersSocket();
@@ -42,12 +52,27 @@ public class Main
             rankClient.connect(rankSocket, echoURI, request);
             rankSocket.awaitClose(5, TimeUnit.SECONDS);
         }
-        for (User user : userList)
-        {
-            System.out.println(user.toString());
-        }
+        Gson gson = new Gson();
+        String body = gson.toJson(userList);
         client.stop();
         rankClient.stop();
-        System.exit(0);
+
+        System.out.println("Sending data to database");
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://10.0.0.130:8080/");
+        HttpEntity entity = new ByteArrayEntity(body.getBytes("UTF-8"));
+        post.setEntity(entity);
+        HttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() >= 200 && response.getStatusLine().getStatusCode() < 300)
+        {
+            System.out.println("Database updated (probably)");
+            System.out.println(response.getStatusLine().toString() + " - " + IOUtils.toString(response.getEntity().getContent()));
+        }
+        else
+        {
+            System.err.println(response.getStatusLine().toString());
+        }
+        System.out.println((System.currentTimeMillis() - startTime)/1000 + " seconds");
     }
 }
